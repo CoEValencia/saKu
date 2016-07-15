f28ca2a98979f9bd23e26fc7e49fc0dbb50f8138
cespanas <cespanas@LES001470.corp.capgemini.com>
2016-07-15 07:59:16 +0100

    Añadidas rutas de imagenes avatar en sql

--- tutorial/src/sql/changes.sql
+++ tutorial/src/sql/changes.sql
@@ -23,8 +23,9 @@ CREATE TABLE AUTHORITY (
 
 /* Tabla USUARIO, passwords hasheados con sha1 iguales al nombre de usuario + id como salt*/
 
-INSERT INTO USERS (ID, USERNAME, PASSWORD, FIN_VIGENCIA_PASS, EMAIL, NOMBRE) VALUES (1, 'demo', sha1('demo{1}'), '2020-12-31', 'demo@domain.com', 'Demo user');
-INSERT INTO USERS (ID, USERNAME, PASSWORD, FIN_VIGENCIA_PASS, EMAIL, NOMBRE) VALUES (2, 'admin', sha1('admin{2}'), '2020-12-31', 'admin@domain.com', 'Administrator');
+INSERT INTO USERS (ID, USERNAME, PASSWORD, FIN_VIGENCIA_PASS, EMAIL, NOMBRE, USERPIC) VALUES (1, 'demo', sha1('demo{1}'), '2020-12-31', 'demo@domain.com', 'Demo user', 'img/cat-icon.png');
+INSERT INTO USERS (ID, USERNAME, PASSWORD, FIN_VIGENCIA_PASS, EMAIL, NOMBRE, USERPIC) VALUES (2, 'admin', sha1('admin{2}'), '2020-12-31', 'admin@domain.com', 'Administrator', 'img/default-icon.png');
+/*INSERT INTO USERS (ID, USERNAME, PASSWORD, FIN_VIGENCIA_PASS, EMAIL, NOMBRE, USERPIC) VALUES (3, 'saku', sha1('saku{3}'), '2020-12-31', 'saku@domain.com', 'Saku', 'img/penguin-icon.png');*/
 
 /* Tabla AUTHORITY*/
 
